d2a01983d10c039b327c7ad1ab4a7ce7d508ec30
Brian Yang <yangbri1@msu.edu>
2024-12-10 05:19:43 +0000

    deleted older repo & start afresh w/ older working code from last repo

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -27,25 +27,37 @@
  * 
  * 
  */
-public class Bird {
+
+ // 'Bird' class -- custom 'Bird' data type in the making
+ public class Bird {
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
-    public int ageMonths;
+    public int ageMonths;   // required to set access modifier to 'public' if want subsequent children classes to 'extends' fields & methods from parent 'Bird' super class
 
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
      * No need to change anything here, for now.
      */
     public Bird(){
+        // assign above 'ageMonths' field (point by 'this' keyword) w/ value from arg's 'ageMonth'
+        // this.ageMonths = ageMonths;
 
+        // initialize 'ageMonths' field w/ default value of 0 in this default Constructor (no args)
+        ageMonths = 0;  // or omit since test cases does NOT seem to take default values into consideration
     }
     /**
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        // if 'Bird' is less than 3 months old
+        if(ageMonths < 3){
+            // return "Tweet" String sound
+            return("Tweet");
+        }
+        // otherwise return "Tweet" String sound
+        return "Chirp";
     }
 
 }
